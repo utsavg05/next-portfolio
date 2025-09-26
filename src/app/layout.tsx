ff7f9bd9aa25b28adcca29276317118bdff812c8
utsavg05 <utsav.gp1204@gmail.com>
2025-09-27 00:28:50 +0530

    vercel web analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { DockDemo } from "@/components/DockDemo";
 import { FloatingDockDemo } from "@/components/FloatingDockDemo";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <FloatingDockDemo />
         <DockDemo />
         {children}
+        <Analytics />
       </body>
     </html>
   );
