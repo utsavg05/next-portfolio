8bc65a64191da424b9580230aef1d38c82570674
utsavg05 <utsav.gp1204@gmail.com>
2026-03-27 19:50:48 +0530

    added google analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-2ZKXXR7QD2"></script>
+      <script>
+        {
+        `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-2ZKXXR7QD2');
+        `}
+      </script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
