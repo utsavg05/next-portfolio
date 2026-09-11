c07d1338da36d60d1cc1c67865cced47a91134c0
utsavg05 <utsav.gp1204@gmail.com>
2026-09-12 01:35:23 +0530

    updated resume link

--- src/components/pages/hero.tsx
+++ src/components/pages/hero.tsx
@@ -193,7 +193,7 @@ const HeroSection = () => {
       {/* Resume */}
       <motion.div variants={fadeInBlur} className="mt-5 text-sm">
         <Link
-          href="https://drive.google.com/file/d/1OABcOufYAOlPGb0HZNeNXM4tSUhaATyx/view?usp=sharing"
+          href="https://drive.google.com/file/d/1ty0zgraUmULRRiXivdwtmVN3SMnaWRS3/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           className="text-foreground underline decoration-dotted underline-offset-4 hover:text-primary transition-colors"
